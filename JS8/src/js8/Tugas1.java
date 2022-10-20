f8950903287c1e11278ef9075b9bf56c5fd15aa2
BRAINS <112637448+21343038@users.noreply.github.com>
2022-10-20 15:40:45 +0700

    Add files via upload

---
+++ JS8/src/js8/Tugas1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package js8;
+
+/**
+ *
+ * @author Bellevran
+ */
+public class Tugas1 {
+    public static void main(String[] args) {
+        String[] days = {"Monday ","Thuesday ","wednesday ","Thursday ","Friday ","Saturday ","Sunday "};
+        for (int i=0;i<days.length; i++){
+            System.out.print(days[i]);
+        }
+    }
+}
